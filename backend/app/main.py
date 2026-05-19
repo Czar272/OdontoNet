4cc6d0322eb291e401e803d6205df2aa31ae91f7
Czar272 <pablo2003cesar@gmail.com>
2026-05-19 16:25:01 -0600

    feat: adjust backend main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,17 +1,15 @@
 from fastapi import FastAPI
 from app.database.connection import Base, engine
-from app.models.patient import Patient
-
-from app.api.patient_routes import router as patient_router
+from app.models.user import User
+from app.api.auth_routes import router as auth_router
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title="OdontoNet API", version="1.0.0")
+app = FastAPI(title="OdontoNet API")
+
+app.include_router(auth_router)
 
 
 @app.get("/")
 def root():
     return {"message": "OdontoNet API running"}
-
-
-app.include_router(patient_router)
