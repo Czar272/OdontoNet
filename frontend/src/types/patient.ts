a97d195425619cc18003b6c2fab2dc4e973feeca
Czar272 <pablo2003cesar@gmail.com>
2026-05-20 18:15:12 -0600

    chore: add patient type file

---
+++ frontend/src/types/patient.ts
@@ -0,0 +1,18 @@
+export interface Patient {
+  id: number;
+  first_name: string;
+  last_name: string;
+  birth_date?: string | null;
+  phone?: string | null;
+  email?: string | null;
+  address?: string | null;
+}
+
+export interface CreatePatientDTO {
+  first_name: string;
+  last_name: string;
+  birth_date?: string;
+  phone?: string;
+  email?: string;
+  address?: string;
+}
