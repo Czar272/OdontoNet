abc1af6444a40f4aea52b552f5834fb3f1ed67d2
Czar272 <pablo2003cesar@gmail.com>
2026-05-20 18:48:52 -0600

    feat: create basic button for future use

---
+++ frontend/src/components/basic/Button.tsx
@@ -0,0 +1,33 @@
+import clsx from "clsx";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: "primary" | "secondary" | "danger";
+}
+
+export default function Button({
+  children,
+  variant = "primary",
+  className,
+  ...props
+}: ButtonProps) {
+  return (
+    <button
+      className={clsx(
+        "px-5 py-3 rounded-xl font-medium transition",
+
+        {
+          "bg-gray-900 text-white hover:bg-gray-800": variant === "primary",
+
+          "bg-gray-200 hover:bg-gray-300": variant === "secondary",
+
+          "bg-red-600 text-white hover:bg-red-700": variant === "danger",
+        },
+
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
