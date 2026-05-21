b2d0459a1a58d2875e5aac249301ef43b17c391c
Czar272 <pablo2003cesar@gmail.com>
2026-05-20 18:08:59 -0600

    feat: config react query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,11 +5,16 @@ import App from "./App";
 
 import "./index.css";
 import { AuthProvider } from "./context/AuthContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </React.StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <React.StrictMode>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </React.StrictMode>
+  </QueryClientProvider>,
 );
